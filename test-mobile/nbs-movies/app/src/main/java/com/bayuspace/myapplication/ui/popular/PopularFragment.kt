237cd1866ead616717f6c02ef781a62221438534
bayu <bayuteguhpamuji@gmail.com>
2021-07-08 06:27:36 +0700

    [bayu] - modify color search view

--- test-mobile/nbs-movies/app/src/main/java/com/bayuspace/myapplication/ui/popular/PopularFragment.kt
+++ test-mobile/nbs-movies/app/src/main/java/com/bayuspace/myapplication/ui/popular/PopularFragment.kt
@@ -1,10 +1,12 @@
 package com.bayuspace.myapplication.ui.popular
 
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.bayuspace.myapplication.databinding.FragmentPopularBinding
@@ -27,6 +29,10 @@ class PopularFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.svPopular.apply {
             val color = ContextCompat.getColor(requireContext(), android.R.color.darker_gray)
+            findViewById<ImageView>(androidx.appcompat.R.id.search_close_btn).setColorFilter(
+                color,
+                PorterDuff.Mode.SRC_IN
+            )
             findViewById<EditText>(androidx.appcompat.R.id.search_src_text).apply {
                 setTextColor(color)
                 setHintTextColor(color)
