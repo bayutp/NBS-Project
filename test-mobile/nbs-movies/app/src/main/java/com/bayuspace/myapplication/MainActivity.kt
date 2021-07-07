cd027c276eb564a88bda9abfea1dff490a297c2a
bayu <bayuteguhpamuji@gmail.com>
2021-07-07 14:43:51 +0700

    [bayu] - add assets

--- test-mobile/nbs-movies/app/src/main/java/com/bayuspace/myapplication/MainActivity.kt
+++ test-mobile/nbs-movies/app/src/main/java/com/bayuspace/myapplication/MainActivity.kt
@@ -2,10 +2,13 @@ package com.bayuspace.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bayuspace.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
