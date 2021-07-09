80b2f4f50aee915a6588916793b7725afec7fb16
bayu <bayuteguhpamuji@gmail.com>
2021-07-09 09:35:53 +0700

    [bayu] - implement remove / add favorites

--- test-mobile/nbs-movies/app/src/main/java/com/bayuspace/myapplication/ui/detail/DetailActivity.kt
+++ test-mobile/nbs-movies/app/src/main/java/com/bayuspace/myapplication/ui/detail/DetailActivity.kt
@@ -2,10 +2,13 @@ package com.bayuspace.myapplication.ui.detail
 
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bayuspace.myapplication.BuildConfig
+import com.bayuspace.myapplication.R
 import com.bayuspace.myapplication.base.BaseActivity
 import com.bayuspace.myapplication.databinding.ActivityDetailBinding
+import com.bayuspace.myapplication.model.entity.MovieEntity
 import com.bayuspace.myapplication.ui.home.HomeFragment.Companion.KEY_MOVIE
 import com.bayuspace.myapplication.utils.gone
 import com.bayuspace.myapplication.utils.loadImage
@@ -17,6 +20,7 @@ class DetailActivity : BaseActivity() {
     private lateinit var binding: ActivityDetailBinding
     private lateinit var adapterCasting: CastingAdapter
     private val detailViewModel: DetailViewModel by viewModel()
+    private var isBookmarked = false
 
     override fun onViewReady(savedInstanceState: Bundle?) {
         binding = ActivityDetailBinding.inflate(layoutInflater)
@@ -41,6 +45,7 @@ class DetailActivity : BaseActivity() {
             with(detailViewModel) {
                 getDetailMovie(id)
                 getMovieCastings(id)
+                checkBookmarked(id)
             }
         }
 
@@ -48,35 +53,53 @@ class DetailActivity : BaseActivity() {
 
     override fun observeData() {
         with(detailViewModel) {
-            observeDetailMovie().onResult {
+            observeDetailMovie().onResult { data ->
                 with(binding) {
-                    val hours = it.runtime / 60
-                    val minutes = it.runtime % 60
+                    val hours = data.runtime / 60
+                    val minutes = data.runtime % 60
                     var genre = ""
-                    it.genres.forEachIndexed { index, data ->
-                        genre += if (index != it.genres.lastIndex) "${data.name} - " else data.name
+                    data.genres.forEachIndexed { index, dataGenre ->
+                        genre += if (index != data.genres.lastIndex) "${dataGenre.name} - " else dataGenre.name
                     }
 
-                    ivMovieDetail.loadImage("${BuildConfig.IMAGE_BASE_URL}${it.posterPath}")
-                    tvTitle.text = it.title
+                    ivMovieDetail.loadImage("${BuildConfig.IMAGE_BASE_URL}${data.posterPath}")
+                    tvTitle.text = data.title
                     tvTimes.text = if (hours < 1) "$minutes m" else "$hours h $minutes m"
                     tvCategory.text = genre
-                    tvDescription.text = it.overview
+                    tvDescription.text = data.overview
+
+                    btnFavorites.setOnClickListener {
+                        detailViewModel.setBookmarked(
+                            listOf(
+                                MovieEntity.mapToMovieEntity(
+                                    data,
+                                    !isBookmarked
+                                )
+                            ), !isBookmarked
+                        )
+                    }
                 }
             }
             observeMovieCastings().onResult {
                 adapterCasting.setData(it.cast)
             }
+            observeSetBookmarked().onResult {
+                isBookmarked = it
+                changeButton(isBookmarked)
+                showMsg("${if (isBookmarked) "add to" else "remove from"} favorites")
+            }
+            observeCheckBookmarked().onResult {
+                isBookmarked = it
+                changeButton(it)
+            }
             observeLoading().onResult {
-                if (it) {
-                    binding.apply {
+                binding.apply {
+                    if (it) {
                         pbDetail.visible()
                         btnFavorites.gone()
                         btnTrailer.gone()
                         tvCasting.gone()
-                    }
-                } else {
-                    binding.apply {
+                    } else {
                         pbDetail.gone()
                         btnFavorites.visible()
                         btnTrailer.visible()
@@ -94,6 +117,36 @@ class DetailActivity : BaseActivity() {
                     it.msg ?: "Terjadi kesalahan! silahkan coba beberapa saat lagi :("
                 )
             }
+        }
+    }
+
+    private fun changeButton(state: Boolean) {
+        with(binding.btnFavorites) {
+            if (state) {
+                setCompoundDrawablesWithIntrinsicBounds(
+                    ContextCompat.getDrawable(
+                        this@DetailActivity,
+                        R.drawable.ic_delete
+                    ), null, null, null
+                )
+                text = getString(R.string.remove_from_fav)
+                backgroundTintList =
+                    ContextCompat.getColorStateList(
+                        this@DetailActivity,
+                        android.R.color.holo_red_dark
+                    )
+            } else {
+                setCompoundDrawablesWithIntrinsicBounds(
+                    ContextCompat.getDrawable(
+                        this@DetailActivity,
+                        R.drawable.ic_add
+                    ), null, null, null
+                )
+                text = getString(R.string.add_to_favorite)
+                backgroundTintList =
+                    ContextCompat.getColorStateList(this@DetailActivity, R.color.blue)
+            }
+
         }
     }
 
--- test-mobile/nbs-movies/app/src/main/java/com/bayuspace/myapplication/ui/detail/DetailViewModel.kt
+++ test-mobile/nbs-movies/app/src/main/java/com/bayuspace/myapplication/ui/detail/DetailViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.bayuspace.myapplication.base.BaseViewModel
 import com.bayuspace.myapplication.base.ResourceState
+import com.bayuspace.myapplication.model.entity.MovieEntity
 import com.bayuspace.myapplication.model.response.MovieCastingResponse
 import com.bayuspace.myapplication.model.response.MovieDetailResponse
 import com.bayuspace.myapplication.repository.DataRepository
@@ -14,9 +15,13 @@ import kotlinx.coroutines.launch
 class DetailViewModel(private val repo: DataRepository) : BaseViewModel() {
     private val onGetDetailMovieSuccess = MutableLiveData<MovieDetailResponse>()
     private val onGetMovieCastingsSuccess = MutableLiveData<MovieCastingResponse>()
+    private val onCheckBookmarkedSuccess = MutableLiveData<Boolean>()
+    private val onSetBookmarkedSuccess = MutableLiveData<Boolean>()
 
     fun observeDetailMovie(): LiveData<MovieDetailResponse> = onGetDetailMovieSuccess
     fun observeMovieCastings(): LiveData<MovieCastingResponse> = onGetMovieCastingsSuccess
+    fun observeCheckBookmarked(): LiveData<Boolean> = onCheckBookmarkedSuccess
+    fun observeSetBookmarked(): LiveData<Boolean> = onSetBookmarkedSuccess
 
     fun getDetailMovie(id: Int) {
         isLoading.postValue(true)
@@ -65,4 +70,32 @@ class DetailViewModel(private val repo: DataRepository) : BaseViewModel() {
             }
         }
     }
+
+    fun setBookmarked(data: List<MovieEntity>, isBookmarked: Boolean) {
+        viewModelScope.launch {
+            when (val state = repo.saveBookmark(data)) {
+                is ResourceState.Success -> onSetBookmarkedSuccess.postValue(isBookmarked)
+
+                is ResourceState.Error -> errorResponse.postValue(state.error.errorData)
+
+                is ResourceState.Loading -> isLoading.postValue(true)
+            }
+        }
+    }
+
+    fun checkBookmarked(id: Int) {
+        viewModelScope.launch {
+            when (val state = repo.checkMovieBookmarked(id)) {
+                is ResourceState.Success -> onCheckBookmarkedSuccess.postValue(
+                    state.result.data?.isBookmark ?: false
+                )
+
+                is ResourceState.Error -> errorResponse.postValue(state.error.errorData)
+
+                is ResourceState.Loading -> isLoading.postValue(true)
+            }
+        }
+    }
+
+
 }
